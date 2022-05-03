01b9258ddfcfa18c468ea10263facf578bf2dcef
Sourav Singh <singhsourav930@gmail.com>
2022-05-03 21:13:42 +0530

    - created landing page

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Features',
+  },
+  {
+    path: 'about',
+    label: 'About',
+  },
+  {
+    path: 'support',
+    label: 'Support',
+  },
+];
